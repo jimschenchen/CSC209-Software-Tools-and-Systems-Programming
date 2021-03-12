3acbe5c8d513ace960c56e77e263a1288c369879
Chenyue <chenyue.li@mail.utoronto.ca>
2021-03-12 23:50:19 +0800

    Update

--- lab8/checkpasswd.c
+++ lab8/checkpasswd.c
@@ -32,6 +32,91 @@ int main(void) {
   }
   
   // TODO
+  // display overlength
+  if (strlen(user_id) > 11) {
+    printf(NO_USER);
+    exit(0);
+  }
+  if (strlen(password) > 11) {
+    printf(INVALID);
+    exit(0);
+  }
 
+  int fd[2], r;
+  
+  if (pipe(fd) == -1) {
+    perror("pipe");
+    exit(1);
+  }
+
+  if ((r = fork()) > 0) { // Parent
+    // close fd[0]
+    if (close(fd[0]) == -1) {
+      perror("close");
+      exit(1);
+    };
+    // Reset STDOUT to pipe
+    // if (dup2(fd[1], STDOUT_FILENO) == -1) {
+    //   perror("dup2");
+    //   exit(1);
+    // }
+    // close fd[1]
+    if (close(fd[1]) == -1) {
+      perror("close");
+      exit(1);
+    };
+
+    // wait for child
+    int status;
+    if (wait(&status) == -1) {
+      perror("wait");
+      exit(1);
+    }
+    if (WIFEXITED(status)) {
+      if (WEXITSTATUS(status) == 0) {
+        printf(SUCCESS);
+      } else if (WEXITSTATUS(status) == 2) {
+        printf(INVALID);
+      } else if (WEXITSTATUS(status) == 3) {
+        printf(NO_USER);
+      } else {
+        exit(1);
+      }
+    }
+
+  } else if (r == 0) {  // Child
+    // Reset STDIN to pipe
+    if (dup2(fd[0], STDIN_FILENO) == -1) {
+      perror("dup2");
+      exit(1);
+    }
+
+    // close fd[0]
+    if (close(fd[0]) == -1) {
+      perror("close");
+      exit(1);
+    }
+
+    // write
+    if (write(fd[1], user_id, 10) == -1) {
+      perror("write");
+    }
+    if (write(fd[1], password, 10) == -1) {
+      perror("write");
+    }
+
+    // close fd[1]
+    if (close(fd[1]) == -1) {
+      perror("close");
+      exit(1);
+    };
+
+    if (execl("./validate", "validate", NULL) == -1) {
+      perror("execl");
+    }
+  } else { // fork err
+    perror("fork");
+    exit(1);
+  }
   return 0;
 }
