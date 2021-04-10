0cf8c938165a88f909951977b4f1c9a3173e9540
Chenyue <chenyue.li@mail.utoronto.ca>
2021-04-10 23:06:31 +0800

    Complete
    bug: 1. name not correctly display

--- a4/auction_client.c
+++ a4/auction_client.c
@@ -141,7 +141,11 @@ void print_auctions(struct auction_data *a, int size) {
      * The array may have some elements where the auction has closed and
      * should not be printed.
      */
-    
+    for (int i = 0; i < MAX_AUCTIONS; i++) {
+        if (a[i].sock_fd > -1) {
+            printf("(%d) %s bid = %d\n", i, a[i].item, a[i].current_bid);
+        }
+    }
     
 }
 
@@ -152,10 +156,71 @@ void print_auctions(struct auction_data *a, int size) {
 void update_auction(char *buf, int size, struct auction_data *a, int index) {
     
     // TODO: Complete this function
-    
+    // server : "%s %d %ld", a->item, a->highest_bid, t->tv_sec
+    char *ptr = NULL;
+    int seconds;
+
+    ptr = strtok(buf, " "); // first word in buf
+    if(ptr != NULL) {
+        if (a[index].item[0] == '\0') {
+            strncpy(a[index].item, ptr, BUF_SIZE);
+        }
+    } else {
+        fprintf(stderr, "ERROR malformed bid: %s", buf);
+        return;
+    }
+
+    ptr = strtok(NULL, " "); // second word in buf
+    if(ptr != NULL) {
+        a[index].current_bid = strtol(ptr, NULL, 10);
+    } else {
+        fprintf(stderr, "ERROR malformed bid: %s", buf);
+        return;
+    }
+
+    ptr = strtok(NULL, " "); // third word in buf
+    if(ptr != NULL) {
+        seconds = strtol(ptr, NULL, 10);
+    } else {
+        fprintf(stderr, "ERROR malformed bid: %s", buf);
+        return;
+    }
+
+    printf("\nNew bid for %s [%d] is %d (%d seconds left)\n", a[index].item, a[index].sock_fd, a[index].current_bid, seconds);
+
     // fprintf(stderr, "ERROR malformed bid: %s", buf);
     // printf("\nNew bid for %s [%d] is %d (%d seconds left)\n",           );
 }
+
+// helper funciton for handling add
+// return the fd
+int handle_add (struct auction_data *a, char *hostname, int port, char *name) {
+    // search the available auctions
+    int auction_index = 0;
+    while (auction_index < MAX_AUCTIONS && a[auction_index].sock_fd != -1) {
+        auction_index++;
+    }
+    if (auction_index == MAX_AUCTIONS) {
+        fprintf(stderr, "client: max auction connections\n");
+        return -1;
+    }
+
+    // do not need to check the bad inputs
+    int fd = add_server(hostname, port);
+    if (fd < 0) {
+        fprintf(stderr, "client: add_server");
+        close(fd);
+        exit(1);
+    }
+    // add sock fd to a
+    a[auction_index].sock_fd = fd;
+
+    // write name to server
+    write(a[auction_index].sock_fd, name, sizeof(name));
+
+    return fd;
+}
+
 
 int main(void) {
 
@@ -163,6 +228,12 @@ int main(void) {
 
     // Declare and initialize necessary variables
     // TODO
+    struct auction_data a[MAX_AUCTIONS];
+    for (int i = 0; i < MAX_AUCTIONS; i++) {
+        a[i].current_bid = -1;
+        a[i].item[0] = '\0';
+        a[i].sock_fd = -1;
+    }
 
     // Get the user to provide a name.
     printf("Please enter a username: ");
@@ -175,11 +246,90 @@ int main(void) {
     print_menu();
 
     // TODO
-    
+    // init select
+    int max_fd = STDIN_FILENO;
+    fd_set all_fds;
+    FD_ZERO(&all_fds);
+    FD_SET(STDIN_FILENO, &all_fds);
 
     while(1) {
         print_prompt();
         // TODO
+
+        // copy all fds
+        fd_set listen_fds = all_fds;
+        int nready;
+        if ((nready = select(max_fd + 1, &listen_fds, NULL, NULL, NULL)) == -1) {
+            perror("client: select");
+            exit(1);
+        }
+
+        // read from STDIN_FILENO
+        if (FD_ISSET(STDIN_FILENO, &listen_fds)) {
+            char command[BUF_SIZE];
+            char arg1[BUF_SIZE];
+            char arg2[BUF_SIZE];
+            int num_read = read(STDIN_FILENO, command, BUF_SIZE);
+            if(num_read <= 0){
+                fprintf(stderr, "ERROR: read from stdin failed\n");
+                exit(1);
+            }
+            int res = parse_command(command, sizeof(command), arg1, arg2);
+            switch(res) {
+                case SHOW: 
+                    print_auctions(a, sizeof(a));
+                    break;
+                case QUIT:
+                    for (int i = 0; i < MAX_AUCTIONS; i++) {
+                        // close all open fd
+                        if (a[i].sock_fd != -1) {   
+                            close(a[i].sock_fd);
+                            a[i].sock_fd = -1;
+                        }
+                    }
+                    exit(0);
+                    // break;
+                case ADD: {
+                    int fd = handle_add(a, arg1, strtol(arg2, NULL, 10), name);
+                    if (fd > -1) {
+                        FD_SET(fd, &all_fds);
+                        max_fd = fd > max_fd ? fd : max_fd;
+                    }     
+                    break;  
+                }
+                case BID: {
+                    int item_index = strtol(arg1, NULL, 10);
+                    int bid_value = strtol(arg2, NULL, 10);
+                    char buf[BUF_SIZE];
+                    sprintf(buf, "%d", bid_value);
+                    write(a[item_index].sock_fd, buf, sizeof(buf));
+                    break;
+                }
+                default:
+                    fprintf(stderr, "Usage: auction_server [-v] [-t timeout] [-p port] item\n");
+                    exit(1);
+            }
+        }
+        
+        // all auctions
+        for (int i = 0; i < MAX_AUCTIONS; i++) {
+            int server_status = -1;
+            if (a[i].sock_fd > -1 && FD_ISSET(a[i].sock_fd, &listen_fds)) {
+                char buf[BUF_SIZE];
+                server_status = read(a[i].sock_fd, buf, sizeof(buf));
+                if (server_status < 0) {
+                    perror("read");
+                    exit(0);
+                }
+                update_auction(buf, sizeof(buf), a, i);
+            }
+
+            // server disconnect
+            if (server_status == 0) {
+                FD_CLR(a[i].sock_fd, &all_fds);
+            }
+        }
+
     }
     return 0; // Shoud never get here
 }
